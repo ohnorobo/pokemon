4d3c434f85198f1974764d798f3c91d0fbeae0c7
Cosi <Cosi@Cosis-ProBook2.local>
2015-02-07 17:45:56 -0500

    functioning randomization of poke names and images
    gitignore for ‘site’ directory
    copied original sprites to flask ‘static’ folder
    original and funny names saved to ‘site’ folder

--- site/__init__.py
+++ site/__init__.py
@@ -1,9 +1,26 @@
 import flask
-app = flask.Flask(__name__)
+import random
+
+app = flask.Flask(__name__, static_folder="/var/www/pokemon/html/static")
+
+
+names = [line.strip() for line in open("../pokemon_names.txt", "r")]
+
+
+def randomize_name():
+    return random.choice(names)
+
+
+def random_pokemon():
+    numberstring = "{:0>3d}.png".format(random.choice(range(0, 151)))
+    return numberstring
 
 @app.route('/')
 def index():
-	return flask.render_template("index.html", pokename="charizard")
+    return flask.render_template("index.html", pokename=randomize_name(), pokemon=random_pokemon())
 
 if __name__ == '__main__':
-	app.run(host='127.0.0.1', port=8081, debug=True)
+    host = "127.0.0.1"
+    port = 8081
+    app.debug = True
+    app.run(host, port)
