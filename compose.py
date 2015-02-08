951361842d6df05c2c1b36629adccd8f9ba64581
ohnorobo <sar.laplante@gmail.com>
2015-02-07 20:43:01 -0500

    moving compise

--- compose.py
+++ compose.py
@@ -27,33 +27,34 @@ def get_pinhole_match(paste, pinhole_a, pinhole_b, imagea, imageb):
   return (x, y)
 
 
+def generate_image():
 
-bodies, heads = get_metadata()
-body = random.choice(bodies)
-head = random.choice(heads)
+  bodies, heads = get_metadata()
+  body = random.choice(bodies)
+  head = random.choice(heads)
 
-pprint(body)
-pprint(head)
-
-
-body_image = Image.open(file_loc+body["filename"])
-head_image = Image.open(file_loc+head["filename"])
-
-master = Image.new(
-    mode='RGBA',
-    size=(1000, 1000),
-    color=(0,0,0,0))
-
-body_pinhole = body["needs"][0]["pinhole"]
-head_pinhole = head["pinhole"]
+  pprint(body)
+  pprint(head)
 
 
-master.paste(body_image,(500, 500))
-coords = get_pinhole_match((500, 500),
-                            body_pinhole, head_pinhole,
-                            body_image, head_image)
-master.paste(head_image, coords, head_image)
-master.save("horror.png")
+  body_image = Image.open(file_loc+body["filename"])
+  head_image = Image.open(file_loc+head["filename"])
+
+  master = Image.new(
+      mode='RGBA',
+      size=(1000, 1000),
+      color=(0,0,0,0))
+
+  body_pinhole = body["needs"][0]["pinhole"]
+  head_pinhole = head["pinhole"]
+
+
+  master.paste(body_image,(500, 500))
+  coords = get_pinhole_match((500, 500),
+                              body_pinhole, head_pinhole,
+                              body_image, head_image)
+  master.paste(head_image, coords, head_image)
+  master.save("horror.png")
 
 
 
