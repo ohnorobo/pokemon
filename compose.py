114f178ad1b92b85aaf70ac3a7fc22fce374a656
ohnorobo <sar.laplante@gmail.com>
2015-02-07 22:23:07 -0500

    changing compose to crop correctly

--- compose.py
+++ compose.py
@@ -27,6 +27,32 @@ def get_pinhole_match(paste, pinhole_a, pinhole_b, imagea, imageb):
   return (x, y)
 
 
+def crop(img):
+    data = img.load()
+    l,w = img.size
+    print l,w
+    a = (l,w)
+    b = (l,0)
+    c = (0,w)
+    d = (0,0)
+
+    print((a, b, c, d))
+
+    for x in range(0,l-1):
+        for y in range(0,w-1):
+            if data[x,y][3] != 0:
+                a = (min(a[0],x),min(a[1],y))
+                b = (min(b[0],x),max(b[1],y))
+                c = (max(c[0],x),min(c[1],y))
+                d = (max(d[0],x),max(d[1],y))
+
+    print((a, b, c, d))
+
+    a = (a[0] -1, a[1]-1)
+    d = (d[0] +1,d[1]+1)
+
+    return img.crop(a+d)
+
 def generate_image():
 
   bodies, heads = get_metadata()
@@ -42,18 +68,19 @@ def generate_image():
 
   master = Image.new(
       mode='RGBA',
-      size=(1000, 1000),
+      size=(300, 300),
       color=(0,0,0,0))
 
   body_pinhole = body["needs"][0]["pinhole"]
   head_pinhole = head["pinhole"]
 
 
-  master.paste(body_image,(500, 500))
-  coords = get_pinhole_match((500, 500),
+  master.paste(body_image,(50, 50))
+  coords = get_pinhole_match((50, 50),
                               body_pinhole, head_pinhole,
                               body_image, head_image)
   master.paste(head_image, coords, head_image)
+  master=crop(master)
   master.save("../site/static/imgs/horror.png")
 
 
