5c0bad1f5c21a7e219c879435118cf9dcdaf383e
Sarah Laplante <sar.laplante@gmail.com>
2016-04-19 02:08:32 -0400

    changing init to use host and port from heroku if given

--- site/__init__.py
+++ site/__init__.py
@@ -108,8 +108,15 @@ def index():
              description=description.decode('utf-8'))
 
 
-if __name__ == '__main__':
-    host = "127.0.0.1"
-    port = 8081
-    #app.debug = True
-    app.run(host, port)
+if __name__ == "__main__":
+    app.debug = True
+
+    host="127.0.0.1"
+    port=80    #run on 80 by default
+
+    if sys.argv[1]: #run on port given from heroku
+        host = sys.argv[1]
+    if sys.argv[2]: #run on host given from heroku
+        port = sys.argv[2]
+
+    app.run(host=host, port=int(port))
